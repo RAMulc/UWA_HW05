4919931e33cde544dca4e0a80c9eeb15dcd6d77f
Trebor <23312931+RAMulc@users.noreply.github.com>
2020-09-26 16:23:33 +0800

    script.js - comments added, function 'updatePage' refactored

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -1,9 +1,12 @@
-
+// Set timelimits for working day. Currently hardcoded. 
+// Future versions could request the user to enter start and end times.
 var startTime = moment("09:00am", "hh:mma");
 var endTime = moment("10:00pm", "hh:mma");
 
+// Date and time for this moment in time.
 var currentTime = moment();
 
+// Function to create timeslots for the working day.
 function createSingleTimeSlot(timeSlotHour, txtAreaStyle) {
     var divRow = $("<div>");
     divRow.addClass("row");
@@ -20,8 +23,10 @@ function createSingleTimeSlot(timeSlotHour, txtAreaStyle) {
     var txtArea = $("<textarea>");
     txtArea.addClass("col-10 textarea")
     txtArea.addClass("todo-text-" + timeSlotHour.toString())
+    // Class to determine row styling for past, present and future.
     txtArea.addClass(txtAreaStyle)
     txtArea.attr("type", "text");
+    // Reinstate text content from localstorage after a refresh, browser close.
     txtArea.text(getFromLocalStorage("todo-" + timeSlotHour.toString()));
     divRow.append(txtArea);
 
@@ -36,6 +41,7 @@ function createSingleTimeSlot(timeSlotHour, txtAreaStyle) {
     divCol2.append(pSave);
 }
 
+// Function to determine weher a timeslot is in the past, present or future.
 function txtAreaCol(timeSlot) {
     if (timeSlot.hour() < currentTime.hour()) {
         return "past";
@@ -48,15 +54,24 @@ function txtAreaCol(timeSlot) {
     }
 }
 
+// Function to refresh the timeslots on the hour and maintain the date and time on the page.
 function updatePage() {
     setInterval(function () {
         setCurrentDate();
         if (moment().minute() === 0 && moment().second() === 0) {
-            location.reload();
+            var lastHour = parseInt(moment().format("h")) - 1;
+            lastHour += moment().format("A");
+            var thisHour = moment().format("hA");
+
+            $(".todo-text-" + lastHour).removeClass("present");
+            $(".todo-text-" + lastHour).addClass("past");
+            $(".todo-text-" + thisHour).removeClass("future");
+            $(".todo-text-" + thisHour).addClass("present");
         }
     }, 1000);
 }
 
+// Function to iterate through timeslot hours when setting up the page.
 function timeSlotHours() {
     for (var i = startTime; i.isBefore(endTime); i.add(1, "hours")) {
         if (moment().format("DD/MM/YYYY") !== getFromLocalStorage("today")) {
@@ -64,31 +79,37 @@ function timeSlotHours() {
         }
         createSingleTimeSlot(i.format("hA"), txtAreaCol(i));
     }
+    // Store todays date in localStorage.
     localStorage.setItem("today", moment().format("DD/MM/YYYY"));
 }
 
+// Fuction to retrieve value for a particular keyName.
 function getFromLocalStorage(keyName) {
     var storedText = localStorage.getItem(keyName);
-    console.log(keyName, storedText);
     if (storedText !== null) {
         return storedText;
     }
     return "";
 }
 
+// Function for saving timeslot tasks.
 function saveTodo() {
     var hourID = $(this).attr("id");
     var todoText = $(".todo-text-" + hourID).val();
-    console.log("todo-text", todoText);
     localStorage.setItem("todo-" + hourID, todoText);
 }
 
+// Current date and time formatted as string.
 function setCurrentDate() {
     $("#currentDay").text(moment().format("DD MMMM YYYY HH:mm:ss"));
 }
 
+// Click event watching save icons in the timeslots to save text content.
 $(document).on("click", ".saveBtn", saveTodo);
 
+// Initialise page with all timeslots and formatting.
 timeSlotHours();
+// Add date and time to page.
 setCurrentDate();
+// Sets timer for time display and refreshing page on the hour
 updatePage();
